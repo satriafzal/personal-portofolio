92991b06f5b6d5da491eb86b91414a6f68a89db3
Satria afzal zaydan <satriafzal@gmail.com>
2026-07-28 20:57:30 +0700

    update about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <section class="about-section py-5" id="about">
     <div class="container">
       <div class="row">
-        <div class="col-12 col-md-10 col-lg-8">
+        <div class="col-12 col-md-10 col-lg-11 col-xl-10">
           <h2 class="dynamic-heading text-white fw-bold mb-3" v-anim="'fade-in'">
             I'm a <span class="text-purple">{{ displayText }}</span><span class="cursor">|</span>
           </h2>
