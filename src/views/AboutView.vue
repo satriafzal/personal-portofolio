5701d1c670c8973188e6233cd7210538ccc33d57
catiulll <satriafzal@gmail.com>
2026-09-06 19:34:09 +0700

    update about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,15 +3,15 @@
     <div class="container">
       <div class="row">
         <div class="col-12 col-md-10 col-lg-9 col-xl-10">
-          <h2 class="dynamic-heading text-white fw-bold mb-3" v-anim="'fade-in'">
+          <h2 class="dynamic-heading text-white fw-bold mb-3" data-aos="fade-in">
             I'm a <span class="text-purple">{{ displayText }}</span><span class="cursor">|</span>
           </h2>
           
-          <p class="current-status text-white mb-2 fs-5" v-anim="'fade-up'">
+          <p class="current-status text-white mb-2 fs-5" data-aos="fade-up">
             Currently, I'm an Informatics Engineering student at <span class="text-primary fw-medium">Universitas Nusa Mandiri</span>.
           </p>
           
-          <p class="description-text fs-5 lh-lg" v-anim="'fade-down'">
+          <p class="description-text fs-5 lh-lg" data-aos="fade-up">
             Hello! I'm Satria Afzal Zaydan, an Informatics Engineering student at Universitas Nusa Mandiri. My lifelong fascination with technology has evolved into a dedicated passion for web development. I specialize in crafting interactive, user-friendly interfaces using Vue.js, seamlessly bridging them with robust backend solutions like Laravel and PHP. Beyond technical execution, I am a highly collaborative team player who thrives in proactive brainstorming and problem-solving sessions. Whether I'm coding, playing music, or gaming, I constantly refine the balance between logic and creativity. I am currently seeking internship opportunities to make a tangible impact, apply my solutions, and grow alongside industry professionals
           </p>
         </div>
