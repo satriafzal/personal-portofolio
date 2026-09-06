603f30b705b076801386f04128b846cabd13e79f
catiulll <satriafzal@gmail.com>
2026-09-06 19:34:36 +0700

    update contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="contact-section py-5" id="contact" v-anim="'fade-in'">
+    <section class="contact-section py-5" id="contact" data-aos="fade-up">
         <div class="container py-5">
 
             <div class="row justify-content-center">
